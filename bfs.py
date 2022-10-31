5bbb046349dc0c22159da86982912f4d28cc4a00
Amal Chawla <chawla.amal.170401@gmail.com>
2022-10-31 21:33:32 +0530

    bfs algorithm with pygame configs

---
+++ bfs.py
@@ -0,0 +1,55 @@
+from constants import *
+
+
+def bfs():
+    bfsQueue = []
+    bfsQueue.append([SOURCE_ROW, SOURCE_COL])
+    distanceFromSrc = []
+    finalPath = []
+    inf = 100000
+
+    for i in range(TOTAL_ROWS):
+        tempRow = []
+        for j in range(TOTAL_COLS):
+            tempRow.append(inf)
+        distanceFromSrc.append(tempRow)
+    distanceFromSrc[SOURCE_ROW][SOURCE_COL] = 0
+
+    while len(bfsQueue) != 0:
+        currentCell = bfsQueue.pop(0)
+        row = currentCell[0]
+        col = currentCell[1]
+
+        if (row == DEST_ROW and col == DEST_COL):
+            break
+
+        dxy = [-1, 0, 1, 0, -1]
+
+        for i in range(4):
+            tx = row + dxy[i]
+            ty = col + dxy[i + 1]
+
+            if not(tx < 0 or tx >= TOTAL_ROWS or ty < 0 or ty >= TOTAL_COLS or distanceFromSrc[tx][ty] != inf or BLOCKED_CELLS[tx][ty] == 1):
+                bfsQueue.append([tx, ty])
+                distanceFromSrc[tx][ty] = distanceFromSrc[row][col] + 1
+
+    if distanceFromSrc[DEST_ROW][DEST_COL] != inf:
+        finalPath = [[DEST_ROW, DEST_COL]]
+        curX = DEST_ROW
+        curY = DEST_COL
+
+        while (curX != SOURCE_ROW or curY != SOURCE_COL):
+            dxy = [-1, 0, 1, 0, -1]
+
+            for i in range(4):
+                tx = curX + dxy[i]
+                ty = curY + dxy[i + 1]
+
+                if not(tx < 0 or tx >= TOTAL_ROWS or ty < 0 or ty >= TOTAL_COLS or distanceFromSrc[tx][ty] != distanceFromSrc[curX][curY] - 1 or BLOCKED_CELLS[tx][ty] == 1):
+                    finalPath.append([tx, ty])
+                    curX = tx
+                    curY = ty
+                    break
+
+    finalPath.reverse()
+    return finalPath
---
+++ project.py
@@ -0,0 +1,79 @@
+from constants import *
+from bfs import bfs
+
+
+def scaleUp(side):
+    return BLOCKSIZE*side
+
+
+def scaleDown(side):
+    return side//BLOCKSIZE
+
+
+def createScaledRect(x, y, color):
+    rect = pygame.Rect(scaleUp(x),
+                       scaleUp(y), BLOCKSIZE, BLOCKSIZE)
+    pygame.draw.rect(SCREEN, color, rect)
+
+
+def createPath(path):
+    if len(path) == 0:
+        return
+
+    path.pop(0)
+    path.pop(-1)
+
+    for cell in path:
+        createScaledRect(cell[0], cell[1], YELLOW)
+        pygame.display.update()
+        pygame.time.Clock().tick(60)
+
+
+def drawGrid():
+    for x in range(0, WINDOW_WIDTH, BLOCKSIZE):
+        for y in range(0, WINDOW_HEIGHT, BLOCKSIZE):
+            rect = pygame.Rect(x, y, BLOCKSIZE, BLOCKSIZE)
+            pygame.draw.rect(SCREEN, BLACK, rect, 1)
+
+
+def main():
+    pygame.init()
+    SCREEN.fill(WHITE)
+    pygame.display.set_caption("PathFinder")
+
+    while True:
+        drawGrid()
+        createScaledRect(SOURCE_ROW, SOURCE_COL, GREEN)
+        createScaledRect(DEST_ROW, DEST_COL, RED)
+
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x = int(pygame.mouse.get_pos()[0] / BLOCKSIZE) * BLOCKSIZE
+                y = int(pygame.mouse.get_pos()[1] / BLOCKSIZE) * BLOCKSIZE
+
+                if pygame.mouse.get_pressed()[0]:
+                    pygame.draw.rect(
+                        SCREEN, BLACK, (x, y, BLOCKSIZE, BLOCKSIZE), 0)
+                    BLOCKED_CELLS[scaleDown(x)][scaleDown(y)] = 1
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    path = bfs()
+
+                else:
+                    pygame.quit()
+                    sys.exit()
+
+                createPath(path)
+
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
